7a801f39eb4cdb306e5d9ed613aafbfe8c8ef265
Glynn Quelch <glynn.quelch@gmail.com>
2023-03-04 22:59:44 +0000

    Using env for test setups

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,10 +17,18 @@ require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 // Give access to tests_add_filter() function.
 require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
+// Load all environment variables into $_ENV
+try {
+	$dotenv = Dotenv\Dotenv::createUnsafeImmutable( __DIR__ );
+	$dotenv->load();
+} catch ( \Throwable $th ) {
+	// Do nothing if fails to find env as not used in pipeline.
+}
+
 tests_add_filter(
 	'muplugins_loaded',
 	function() {
-	
+
 	}
 );
 
